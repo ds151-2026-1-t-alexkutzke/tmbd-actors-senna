a4fb92608d7bebdd0a7b411209e12808bd5356a7
senna <joaopedrorochasenna@gmail.com>
2026-04-01 18:28:05 -0300

    feito

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack
       screenOptions={{
-        headerStyle: { backgroundColor: '#121212' },
-        headerTintColor: '#FFFFFF',
-        headerTitleStyle: { fontWeight: 'bold' },
-        contentStyle: { backgroundColor: '#121212' },
+        headerStyle: { backgroundColor: "#121212" },
+        headerTintColor: "#FFFFFF",
+        headerTitleStyle: { fontWeight: "bold" },
+        contentStyle: { backgroundColor: "#121212" },
       }}
     >
-      <Stack.Screen name="index" options={{ title: 'TMDB Filmes' }} />
-      <Stack.Screen name="movie/[id]" options={{ title: 'Detalhes do Filme' }} />
+      <Stack.Screen name="index" options={{ title: "TMDB Filmes" }} />
+      <Stack.Screen
+        name="movie/[id]"
+        options={{ title: "Detalhes do Filme" }}
+      />
+      <Stack.Screen name="actor/[id]" options={{ title: "Biografia" }} />
     </Stack>
   );
 }
